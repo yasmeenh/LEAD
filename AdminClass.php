19ab2e1654663362616041c1e41fe4035465b21f
Mennah <mennah.rabie@gmail.com>
2016-04-22 15:21:34 +0200

    Update AdminClass.php

--- AdminClass.php
+++ AdminClass.php
@@ -1,13 +1,40 @@
-<?php require 'C:\wamp\www\ModelClass.php';?>
+
+<?php require 'C:\wamp\www\Connection.php';?>
 <?php
-class AdminClass extends ModelClass  
+class AdminClass   
  { 
-    function selectPostsbyAdmin($email) {
-         $query = "select Posts from Post where stnemail = '".$email."'";
-    } 
+ 
+    function selectPostsbyAdmin($email) 
+{
+         $query = "select posts from post where stnemail = '".$email."'";
+		 $resArr = array(); //create the result array
+		 
+		 $db = Connection::getInstance();
+         $mysqli = $db->getConnection(); 
+         $resArr = $mysqli->query($query);
+		 
+		 
+		 
+		 return $resArr;
+		 
+		 
+		 
+}
+	
 	function loadName ($email)
-	{
-		$query = "select Name from Client where Email = '".$email."'";
-	}
+{
+		$query = "select name from stdact where email = '".$email."'";
+		$resArr = array(); //create the result array
+		 
+		 $db = Connection::getInstance();
+         $mysqli = $db->getConnection(); 
+         $resArr = $mysqli->query($query);
+		 
+		 return $resArr;
+		 
+		 
+}
  } 
-?>
+
+
+<?php
