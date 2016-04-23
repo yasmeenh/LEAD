a2041fb55400309e1122f4ee1190d797f4933e26
yasmeen hesham <yasmeen hesham>
2016-04-23 17:20:41 +0200

    Update Controller Class

--- Controller.php
+++ Controller.php
@@ -1,9 +1,71 @@
-<?php require 'C:\wamp\www\AdminClass.php';?>
+<?php 
+ require 'C:\wamp\www\AdminClass.php';
+ require_once("Database.php");
+?>
 
 <?php
 
 class Controller
 {
+
+  private static  $instance=null;
+  private $UserSearch;
+  private $SearchResult=array();
+  protected $Search_Model=null;
+
+  public static  function CreateControllerInstance  (  $sm  ){
+	  if (!self::$instance)
+	  {
+		  self::$instance = new SearchController ($sm); 
+	  }
+	  return self::$instance ;
+  }	
+
+//Constructor which take as a parameter the Search Model object
+  public function __construct($sm) {
+    $this->Search_Model = $sm;
+  }	
+
+  //Function to get the student Activity that user entered in the search box
+  public function GetUserSearch ($us){
+
+    $this->UserSearch=$us;  	
+  }
+//Function to send to Model the student Activity that user entered in the search box
+  public function SendUserSearch (){
+
+  	$this->Search_Model->GetUserSearch($this->UserSearch);
+  }
+//Function to get the search result from Model
+  public function GetUserSearchResult (){
+    if($this->UserSearch=='STP')
+    {
+     
+       require_once("SearchH.php");
+    }
+    else if($this->UserSearch=='IEEE')
+    {
+      require_once("IEEE.php");
+    }
+
+    else if($this->UserSearch=='Meshwar')
+    {
+      require_once("Meshwar.php");
+    }
+
+    else
+    {
+      require_once("NoResults.php");
+    }
+
+  	$this->SearchResult=$this->Search_Model->SendUserSearchResult();
+
+  }
+//Function to send the search result to SearchH.php(Search.html) to echo the array of posts
+  public function SendUserSearchResult (){
+
+    return $this->SearchResult;
+  }
 	
 	function CallLoadName ($email)
 	{
@@ -51,7 +113,6 @@ class Controller
 	    $result = array();
         $result = $AD->UpdatePostByDate($Date ,$Post,$UpdatedPost);	
 	}
-	
 	
 	
 	
